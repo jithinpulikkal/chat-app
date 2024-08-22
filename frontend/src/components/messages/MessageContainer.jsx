ff23450949cf8b6f82c58c57b4b5f8490156b2e7
Jithin <jithinpulikkal875@gmail.com>
2024-08-22 12:45:17 +0530

    padding

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -30,7 +30,7 @@ const NoChatSelected = () => {
     return (
         <div className="flex items-center justify-center w-full h-full">
             <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
-                <img src={Icon} style={{height:50}} alt="icon"/>
+                <img src={Icon} style={{height:50, marginTop:30}} alt="icon"/>
                 <p>Welcome <span className="text-gray-500">John doe</span> </p>
                 <p>Select a contact to start messaging</p>
                 <img src={Robot} style={{height:250}} alt="robot" />
