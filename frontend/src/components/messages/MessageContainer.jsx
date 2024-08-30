8a3012a5697c6dbff068911ebb6f79798e40a875
Jithin <jithinpulikkal875@gmail.com>
2024-08-30 12:44:11 +0530

    zustand for conversation state

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,20 +1,26 @@
 import MessageInput from "./MessageInput";
 import Messages from "./Messages";
-import Robot from "../../assets/robot.gif"
-import Icon from "../../assets/lets-chat-logo-transparent.png"
+import Robot from "../../assets/robot.gif";
+import Icon from "../../assets/lets-chat-logo-transparent.png";
+import useConversation from "../../zustand/useConversatio";
+import { useEffect } from "react";
 
 const MessageContainer = () => {
-    const noChatSelected = true;
+    const { selectedConversation, setSelectedConversation } = useConversation();
+    useEffect(() => {
+        //unmount selected user
+        return () => setSelectedConversation(null);
+    }, [setSelectedConversation]);
     return (
         <div className="md:min-w-[450px] flex flex-col">
-            {noChatSelected ? (
+            {!selectedConversation ? (
                 <NoChatSelected />
             ) : (
                 <>
                     {/* Header  */}
                     <div className="bg-slate-500 px-4 py-2 mb-2 ">
-                        <span className="label-text">To:</span>
-                        <span className="text-gray-900 font-bold"> John Doe</span>
+                        <span className="label-text"> [>] </span>
+                        <span className="text-gray-900 font-bold">{selectedConversation.name}</span>
                     </div>
                     <Messages />
                     <MessageInput />
@@ -30,13 +36,12 @@ const NoChatSelected = () => {
     return (
         <div className="flex items-center justify-center w-full h-full">
             <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
-                <img src={Icon} style={{height:50, marginTop:30}} alt="icon"/>
-                <p>Welcome <span className="text-gray-500">
-                {JSON.parse(localStorage.getItem("chat-user"))?.name}
-                    </span> </p>
+                <img src={Icon} style={{ height: 50, marginTop: 30 }} alt="icon" />
+                <p>
+                    Welcome <span className="text-gray-500">{JSON.parse(localStorage.getItem("chat-user"))?.name}</span>{" "}
+                </p>
                 <p>Select a contact to start messaging</p>
-                <img src={Robot} style={{height:250}} alt="robot" />
-
+                <img src={Robot} style={{ height: 250 }} alt="robot" />
             </div>
         </div>
     );
